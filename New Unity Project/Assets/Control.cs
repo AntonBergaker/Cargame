d045e0000e4f1b00550f7a828c881198441fcdb5
Anton <noten.b@hotmail.com>
2015-09-26 00:22:40 +0200

    CarAI minmap, sounds, mapfixes, respawn

--- New Unity Project/Assets/Control.cs
+++ New Unity Project/Assets/Control.cs
@@ -3,14 +3,28 @@ using System.Collections;
 
 public class Control : MonoBehaviour {
     public Rigidbody Car;
-    public Object car;
+    public GameObject car;
+    public Rigidbody Car2;
+    public GameObject car2;
     public GUIStyle Title;
     public Texture map;
+    public Texture mapDot;
+    public Texture enemyMapDot;
     private float dt = 0;
+    public float mapX;
+    public float mapY;
+    public float mapWidth;
+    public float mapHeight;
+    public bool mapFromRight;
+    public bool mapFromBot;
+
+    private GoForward car2_vars, car1_vars;
 
 	// Use this for initialization
 	void Start () {
         Physics.gravity = new Vector3(0F,-5F,0F);
+        car2_vars = car2.GetComponent<GoForward>();
+        car1_vars = car.GetComponent<GoForward>();
 	}
 	
 	// Update is called once per frame
@@ -20,17 +34,42 @@ public class Control : MonoBehaviour {
 
     void OnGUI()
     {
-        GUI.Label(new Rect(10, 10, 1, 1), "KPH: " + Mathf.Round(Car.velocity.magnitude*15F), Title);
-        GUI.Label(new Rect(10, 30, 1, 1), "Gear: " + GoForward.gear, Title);
-        GUI.Label(new Rect(10, 50, 1, 1), "FPS: " + dt.ToString(), Title);
+        float mx, my;
+        float width = Screen.width;
+        float height = Screen.height;
 
-        GUI.Label(new Rect(10, 70, 1, 1), "X: " + Car.position.x.ToString(), Title);
-        GUI.Label(new Rect(10, 90, 1, 1), "Y: " + Car.position.y.ToString(), Title);
-        GUI.DrawTexture(new Rect(100, 10, 150, 150), map);
+        if (mapFromRight == true)
+        { mx = width - (mapX + mapWidth); }
+        else
+        { mx = mapX; }
 
-        float xx = (Car.position.x/500) * 150;
-        float zz = (Car.position.z/500)* 150;
+        if (mapFromBot == true)
+        { my = height - (mapY + mapHeight); }
+        else
+        { my = mapY; }
 
-        GUI.DrawTexture(new Rect(175+zz, 85+xx, 30,30),map);
+        GUI.Label(new Rect(10, 10, 1, 1), "KPH: " + Mathf.Round(Car.velocity.magnitude*12F), Title);
+        GUI.Label(new Rect(10, 30, 1, 1), "Gear: " + car1_vars.gear, Title);
+        GUI.Label(new Rect(10, 50, 1, 1), "Lap Position: " + car1_vars.raceLoc + "/" + car1_vars.navArraySize, Title);
+        GUI.Label(new Rect(10, 70, 1, 1), "Lap: " + car1_vars.lap, Title);
+
+        GUI.Label(new Rect(200, 10, 1, 1), "KPH: " + Mathf.Round(Car2.velocity.magnitude * 12F), Title);
+        GUI.Label(new Rect(200, 30, 1, 1), "Gear: " + car2_vars.gear, Title);
+        GUI.Label(new Rect(200, 50, 1, 1), "Lap Position: " + car2_vars.raceLoc + "/" + car2_vars.navArraySize, Title);
+        GUI.Label(new Rect(200, 70, 1, 1), "Lap: " + car2_vars.lap, Title);
+        GUI.DrawTexture(new Rect(mx, my, mapWidth, mapHeight), map);
+
+        float xx = (Car2.position.x/500) * mapWidth;
+        float zz = -(Car2.position.z/500) * mapHeight;
+
+        GUI.DrawTexture(new Rect(xx+mx+ mapWidth*0.5F -8, zz+my+ mapHeight*0.5F -8, 16,16),enemyMapDot);
+
+        xx = (Car.position.x / 500) * mapWidth;
+        zz = -(Car.position.z / 500) * mapHeight;
+
+        GUI.DrawTexture(new Rect(xx + mx + mapWidth * 0.5F - 8, zz + my + mapHeight * 0.5F - 8, 16, 16), mapDot);
+
+
+        GUI.Label(new Rect(300, 10, 1, 1), "FPS: " + dt.ToString(), Title);
     }
 }
--- New Unity Project/Assets/GoForward.cs
+++ New Unity Project/Assets/GoForward.cs
@@ -3,6 +3,15 @@ using System.Collections;
 
 public class GoForward : MonoBehaviour
 {
+    public bool isEnemy;
+
+    public GameObject finishLine;
+    public GameObject navGroup;
+    private GameObject[] navArray;
+    public int raceLoc = 0;
+    public int lap = 0;
+    public int navArraySize = 0;
+
     public WheelCollider wheelBR;
     public WheelCollider wheelBL;
     public WheelCollider wheelFR;
@@ -12,8 +21,36 @@ public class GoForward : MonoBehaviour
     public Transform wheelFLMesh;
     public Transform wheelFRMesh;
     public Rigidbody body;
-    public static int gear = 1;
+
+    public Light backLightL;
+    public Light backLightR;
+
+    public int gear = 1;
     public float maxTorque;
+
+    public AudioSource engineSound;
+
+    private int respawntimer = 0;
+
+    private bool aiRecentCollide;
+    private int aiRecentCollideTimer;
+
+    private float brakeBuildup = 0;
+
+    void Start() {
+        navArray = new GameObject[navGroup.transform.childCount];
+        int rep = navGroup.transform.childCount;
+        for (int i = 0; i < rep; i++)
+        {
+            navArray[i] = navGroup.transform.GetChild(i).gameObject;
+
+            navArray[i].GetComponent<MeshRenderer>().enabled = false;
+        }
+        finishLine.GetComponent<MeshRenderer>().enabled = false;
+        navArraySize = navArray.Length;
+        body.centerOfMass = new Vector3(0F,-0.05F,0F);
+    }
+
 
     // Update is called once per frame
     void FixedUpdate()
@@ -22,16 +59,16 @@ public class GoForward : MonoBehaviour
         float stiffness = gear;
         float torque = 1;
         //Change gears!
-        if (speed < 15F)
+        if (speed < 10F)
         {
-            torque = 5;
-            stiffness = 10;
+            torque = 2;
+            stiffness = 7;
             gear = 1;
         }
 
         else if (speed < 30F)
         {
-            torque = 2;
+            torque = 1.5F;
             stiffness = 5;
             gear = 1;
         }
@@ -70,24 +107,120 @@ public class GoForward : MonoBehaviour
         wheelFL.forwardFriction = temp;
         wheelFR.forwardFriction = temp;
 
+        float vermov = 0;
+        float hormov = 0;
+        if (!isEnemy) //If Player Controls
+        {
+            vermov = Mathf.Clamp(Input.GetAxis("Vertical") - Input.GetAxis("Vertical-"), -1F, 1F);
+            hormov = Input.GetAxis("Horizontal");
+        }
+        else //If AI Controls
+        {
+            int reverse = 1;
+            RaycastHit hit;
 
-        wheelBR.motorTorque = torque * Input.GetAxis("Vertical");
-        wheelBL.motorTorque = torque * Input.GetAxis("Vertical");
+            Vector3 origin = body.position;
+            origin.y += 0.20F;
+            //detect collision
+            if (Physics.Raycast(origin, transform.forward, out hit, 0.9F))
+            {
+                aiRecentCollide = true;
+                aiRecentCollideTimer = 15;
+            }
+            var forward = transform.forward;
+            Debug.DrawRay(origin, forward * 0.9F, Color.blue);
+            origin += transform.right * 0.15F;
+
+            //detect collision
+            if (Physics.Raycast(origin, transform.forward, out hit, 0.9F))
+            {
+                aiRecentCollide = true;
+                aiRecentCollideTimer = 15;
+            }
+            Debug.DrawRay(origin, forward * 0.9F, Color.blue);
+            origin -= transform.right*0.3F;
+            
+            //detect collision
+            if (Physics.Raycast(origin, transform.forward, out hit, 0.9F))
+            {
+                aiRecentCollide = true;
+                aiRecentCollideTimer = 15;
+            }
+            Debug.DrawRay(origin, forward * 0.9F, Color.blue);
+            //if collision correction
+            if (aiRecentCollide)
+            {
+                if (aiRecentCollideTimer <= 0)
+                {aiRecentCollide = false;}
+
+                reverse = -1500;
+                aiRecentCollideTimer--;
+            }
+
+            //normal movement
+            Vector3 target = navArray[(raceLoc + 1) % navArray.Length].transform.position;
+
+            Vector3 RelativeWaypointPosition = transform.InverseTransformPoint(new Vector3(target.x,transform.position.y,target.z));
+
+            hormov = (Mathf.Clamp(reverse * RelativeWaypointPosition.x / RelativeWaypointPosition.magnitude, -0.9F, 0.9F));
+            vermov = Mathf.Clamp(reverse*(1-(Mathf.Abs(hormov)*0.6F)),-1F,1F);
+        }
+
+        wheelBR.motorTorque = torque * vermov;
+        wheelBL.motorTorque = torque * vermov;
         float stearMod = Mathf.Clamp(40F-(body.velocity.magnitude*5F),15F,40F);
-        wheelFL.steerAngle = stearMod * Input.GetAxis("Horizontal");
-        wheelFR.steerAngle = stearMod * Input.GetAxis("Horizontal");
+        wheelFL.steerAngle = stearMod * hormov;
+        wheelFR.steerAngle = stearMod * hormov;
 
-        body.AddForce(Input.GetAxis("Vertical") * 100 * body.transform.forward);
-        body.AddForce(-body.transform.up * (body.velocity.magnitude * 100F + 100F));
+        body.AddForce(vermov * 100 * body.transform.forward);
+        body.AddForce(-body.transform.up * (body.velocity.magnitude * 110F + 200F));
 
-        if (wheelBR.isGrounded)
-        { body.AddForce(-body.transform.up * 350F); }
-        if (wheelBL.isGrounded)
-        { body.AddForce(-body.transform.up * 350F); }
-        if (wheelFL.isGrounded)
-        { body.AddForce(-body.transform.up * 250F); }
-        if (wheelFR.isGrounded)
-        { body.AddForce(-body.transform.up * 250F); }
+        int touching = 0;
+
+        if (!wheelBR.isGrounded)
+        {
+            body.AddForce(-body.transform.up * 150F);
+            touching++;
+        }
+        if (!wheelBL.isGrounded)
+        {
+            body.AddForce(-body.transform.up * 150F);
+            touching++;
+        }
+        if (!wheelFL.isGrounded)
+        {
+            body.AddForce(-body.transform.up * 100F);
+            touching++;
+        }
+        if (!wheelFR.isGrounded)
+        {
+            body.AddForce(-body.transform.up * 100F);
+            touching++;
+        }
+        if (touching==4)
+        {
+            respawntimer++; //if you've beeen upside down a while, go back to safespot
+            if (respawntimer>240)
+            {
+                body.isKinematic = true;
+                Vector3 tempV3 = navArray[raceLoc].transform.position;
+                tempV3.z += 1F;
+
+                body.position = tempV3;
+                body.transform.LookAt(navArray[raceLoc + 1].transform);
+
+                Vector3 oldrot = body.rotation.eulerAngles;
+                oldrot.z = 0;
+                body.rotation = Quaternion.Euler(oldrot);
+
+                body.isKinematic = false;
+                enabled = false;
+                enabled = true;
+                respawntimer = 0;
+           }
+        }
+        else
+        { respawntimer = 0;}
 
     }
     void Update()
@@ -109,6 +242,49 @@ public class GoForward : MonoBehaviour
 
         wheelFRMesh.localEulerAngles = temp;
 
-        //extra forces
+        //sounds
+        engineSound.pitch = 1 + body.velocity.magnitude/10F;
+
+        //breaklight
+        if (wheelBL.rpm < 0F && Mathf.Clamp(Input.GetAxis("Vertical") - Input.GetAxis("Vertical-"), -1F, 1F) < 0)
+        {
+            if (brakeBuildup > 1)
+            {
+                backLightL.enabled = true;
+                backLightR.enabled = true;
+            }
+            else
+            { brakeBuildup += Time.deltaTime; }
+        }
+        else
+        {
+            backLightL.enabled = false;
+            backLightR.enabled = false;
+            brakeBuildup = 0;
+        }
+
+        //calculate your position
+        //check up too 12 checkpoints forward
+        for (int i = 0; i < 15; i++)
+        {
+            if (i+raceLoc >= navArray.Length)
+            { break; }
+            if (Vector3.Distance(transform.position, navArray[raceLoc+i].transform.position) < 7.5F)
+            {
+                raceLoc+=i;
+                break;
+            }
+        }
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject == finishLine)
+        {
+            if (raceLoc >= navArray.Length-5)
+            {
+                lap++;
+                raceLoc = 0;
+            }
+        }
     }
 }
