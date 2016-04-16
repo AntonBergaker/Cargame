d50ec49c2397a383f36ab057f22ff8a550e9b466
Anton <noten.b@hotmail.com>
2016-04-17 01:55:19 +0200

    tunnel start

--- New Unity Project/Assets/CarCam.cs
+++ New Unity Project/Assets/CarCam.cs
@@ -34,7 +34,7 @@ public class CarCam : MonoBehaviour
         myAngel = Mathf.LerpAngle(myAngel, wantedAngel, rotationDampening * Time.deltaTime);
         myHeight = Mathf.Lerp(myHeight, wantedHeight, heightDampening * Time.deltaTime);
         if (myHeight < wantedHeight)
-        { myHeight = Mathf.Lerp(myHeight,wantedHeight,0.1F * Time.deltaTime); }
+        { myHeight = Mathf.Lerp(myHeight,wantedHeight,0.2F * Time.deltaTime); }
         var currentRotation = Quaternion.Euler(0, myAngel, 0);
         transform.position = car.position;
         transform.position -= currentRotation * Vector3.forward * distance;
